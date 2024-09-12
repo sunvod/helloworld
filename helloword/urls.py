af946dc1be2bdc387ef48ac36a2298a08f7279a2
sunvod <riccardo.ughi@mail.polimi.it>
2024-09-13 00:48:09 +0200

    A commit

--- helloword/urls.py
+++ helloword/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from contact import views
+
 urlpatterns = [
     path('', views.index, name='index'),  # aggiungi questa riga per la pagina di default
     path('admin/', admin.site.urls),
